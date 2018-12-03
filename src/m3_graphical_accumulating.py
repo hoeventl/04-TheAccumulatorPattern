64df126cbf1c8bfab6fe6ceaf09095ac14741d94
hoeventl <hoeventl@rose-hulman.edu>
2018-12-03 15:25:33 -0500

    uhh

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Thomas Hoevener.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -96,7 +96,7 @@ def draw_parallel_lines(n, point, length, window):
       :type window: rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -109,6 +109,12 @@ def draw_parallel_lines(n, point, length, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ###########################################################################
     # -------------------------------------------------------------------------
+    for k in range(n + 1):
+        end_point = rg.Point(point.x + length, point.y)
+        drawn_line = rg.Line(point, end_point)
+        drawn_line.attach_to(window)
+        point.y = point.y + 30
+    window.render()
 
 
 def run_test_draw_lines():
@@ -174,6 +180,11 @@ def draw_lines(n, point, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ###########################################################################
     # -------------------------------------------------------------------------
+    for k in range(n + 1):
+        end_point = rg.Point(point.x + 100, point.y - k * (100 / n))
+        drawn_line = rg.Line(point, end_point)
+        drawn_line.attach_to(window)
+    window.render()
 
 
 # -----------------------------------------------------------------------------
